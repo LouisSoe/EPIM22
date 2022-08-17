fb67a9e3050653cccf9def86533064bcb1b0bd31
Arallel <mdvitoah@gmail.com>
2022-08-17 15:40:01 +0700

    fix

--- resources/views/user/userprofile.blade.php
+++ resources/views/user/userprofile.blade.php
@@ -1,30 +1,30 @@
 <link href="css/sb-admin-2.css" rel="stylesheet">
 <div class="container">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-12 col-md-9">
-                <div class="card o-hidden rounded border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <div class="text-center">
-                                        <img class="mb-3 mt-2 rounded-circle" src="img/k.jpg" width="125px">
-                                        <h5 class="text-center" style="color:gray;"><b>Louis Hazelnut</b></h5>
-                                            <div class="row justify-content-center">
-                                                <div class="col-xl-12 col-lg-12 col-md-9">
-                                                    <div class="card o-hidden rounded border-0 my-5">
-                                                        <div class="bg-gray-200 card-body p-3">
-                                                        <div class="text-left">NIM:</div> 
-                                                        </div>
-                                                    </div>     
-                                                </div>       
-                                            </div>             
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+        <div class="col-xl-6 col-lg-12 col-md-9">
+            <div class="card o-hidden rounded border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="p-5">
+                        <div class="text-center">
+                            <div class="text-center">
+                                <img class="mb-3 mt-2 rounded-circle" src="img/k.jpg" width="125px">
+                                <h5 class="text-center" style="color:gray;"><b>Louis Hazelnut</b></h5>
+                                <div class="row justify-content-center">
+                                    <div class="col-xl-12 col-lg-12 col-md-9">
+                                        <div class="card o-hidden rounded border-0 my-5">
+                                            <div class="bg-gray-200 card-body p-3">
+                                                <div class="text-left">NIM:</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-</div>
+    </div>
